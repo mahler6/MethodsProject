e7f616fbbcc402fd9ef7c19805638d1f91ed6b9d
ClaireG-J <107225401+ClaireG-J@users.noreply.github.com>
2022-07-20 16:58:35 -0500

    Add files via upload

---
+++ inventory.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jul 20, 2022 at 11:56 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `MethodsProject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `ISBN` varchar(14) NOT NULL,
+  `Title` varchar(50) DEFAULT NULL,
+  `Author` varchar(50) DEFAULT NULL,
+  `Publisher` varchar(50) DEFAULT NULL,
+  `Stock` int(3) DEFAULT NULL,
+  `Price` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`ISBN`, `Title`, `Author`, `Publisher`, `Stock`, `Price`) VALUES
+('9780001831803', 'The Lion The Witch and The Wardrobe', 'C. S. Lewis', 'Geoffrey Bles', 2, 19.29),
+('9780007525508', 'The Hobbit', 'J. R. R. Tolkein', 'George Allen & Unwin', 18, 29.95),
+('9780425123348', 'A Christmas Carol', 'Charles Dickens', 'Chapman & Hall', 5, 10.11),
+('9780545791878', 'The Hunger Games', 'Suzanne Collins', 'Scholastic', 20, 21),
+('9781529046137', 'The Hitchhikers Guide to the Galaxy', 'Douglas Adams', 'Megadodo Publications', 18, 29.95);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`ISBN`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
