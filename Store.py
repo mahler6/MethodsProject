98238007469b834e0ac56842ca864cebf878d243
ClaireG-J <107225401+ClaireG-J@users.noreply.github.com>
2022-07-29 22:52:08 -0500

    Update Store.py
    added __init__ and getter methods for both classes

--- Store.py
+++ Store.py
@@ -2,9 +2,73 @@
 
 class Inventory:
 
-	
+   def __init__(self, title, author, publisher, stock, price, isbn):
+
+      self.Title = title
+
+      self.Author = author
+
+      self.Publisher = publisher
+
+      self.Stock = stock
+
+      self.Price = price
+
+      self.ISBN = isbn
+
+   
+
+   def getTitle(self):
+
+      return self.Title
+
+   def getAuthor(self):
+
+      return self.Title
+
+   def getPublisher(self):
+
+      return self.Title
+
+   def getStock(self):
+
+      return self.Title
+
+   def getPrice(self):
+
+      return self.Title
+
+   def getISBN(self):
+
+      return self.ISBN
+
+   
+
+
 
 class Cart:
+
+   def __init__(self, isbn, quantity, price):
+
+      self.ISBN = isbn
+
+      self.Quantity = quantity
+
+      self.Total = price
+
+
+
+   def getISBN(self):
+
+      return self.ISBN
+
+   def getQuantity(self):
+
+      return self.Quantity
+
+   def getTotal(self):
+
+      return self.Total
 
 
 
