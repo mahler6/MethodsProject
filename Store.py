9b15cc32b8ea6bc393faea6f6e0a785665be728e
ClaireG-J <107225401+ClaireG-J@users.noreply.github.com>
2022-08-01 20:36:54 -0500

    Update Store.py
    Added Connect_inventory and Connect_cart functions to streamline connecting to the sql database.
    Added Checkout function to Cart class. Currently only partially functional, will only be fully functional once the Lower stock function for Inventory is complete.

--- Store.py
+++ Store.py
@@ -42,7 +42,15 @@ class Inventory:
 
       return self.ISBN
 
-   
+   def Connect_inventory():
+      try:
+         connection = mysql.connector.connect(host="localhost" ,user="root", password="", database="MethodsProject")
+         print("Successful connection.")
+         cursor = connection.cursor()
+         return connection, cursor
+      except:
+         print("Failed connection.")
+         sys.exit()   
 
 
 
@@ -70,5 +78,23 @@ class Cart:
 
       return self.Total
 
-
-
+   def Connect_cart():
+      try:
+         connection = mysql.connector.connect(host="localhost",user="root",password="",database="MethodsProject")
+         print("Successful connection.")
+         cursor = connection.cursor()
+         return connection, cursor
+      except:
+         print("Failed connection.")
+         sys.exit()
+   def Checkout():
+      connection, cursor = Cart.Connect_cart()
+      #rows = cursor.execute("SELECT COUNT(ISBN) FROM cart")
+      #for r in rows:
+         #Inventory.lowerstock()
+      cursor.execute("DELETE FROM cart")
+      connection.commit()
+      print(cursor.rowcount, "records deleted, cart cleared")
+      cursor.close()
+      connection.close()
+      print("Connection closed")
